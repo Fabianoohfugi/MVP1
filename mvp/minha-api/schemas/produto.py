977d0270632073ffbc7ecaf89635e0f40b36c38a
Fabianoohfugi <71031819+Fabianoohfugi@users.noreply.github.com>
2023-05-02 04:30:19 -0300

    Update produto.py

--- mvp/minha-api/schemas/produto.py
+++ mvp/minha-api/schemas/produto.py
@@ -6,30 +6,24 @@ from schemas import ComentarioSchema
 
 
 class ProdutoSchema(BaseModel):
-    """ Define como um novo produto a ser inserido deve ser representado
-    """
+    
     nome: str = "Caneta"
     quantidade: Optional[int] = 1
     valor: float = 2.50
 
 
 class ProdutoBuscaSchema(BaseModel):
-    """ Define como deve ser a estrutura que representa a busca. Que será
-        feita apenas com base no nome do produto.
-    """
+    
     nome: str = "Teste"
 
 
 class ListagemProdutosSchema(BaseModel):
-    """ Define como uma listagem de produtos será retornada.
-    """
+    
     produtos:List[ProdutoSchema]
 
 
 def apresenta_produtos(produtos: List[Produto]):
-    """ Retorna uma representação do produto seguindo o schema definido em
-        ProdutoViewSchema.
-    """
+    
     result = []
     for produto in produtos:
         result.append({
@@ -42,8 +36,7 @@ def apresenta_produtos(produtos: List[Produto]):
 
 
 class ProdutoViewSchema(BaseModel):
-    """ Define como um produto será retornado: produto + comentários.
-    """
+    
     id: int = 1
     nome: str = "caneta"
     quantidade: Optional[int] = 10
@@ -53,16 +46,12 @@ class ProdutoViewSchema(BaseModel):
 
 
 class ProdutoDelSchema(BaseModel):
-    """ Define como deve ser a estrutura do dado retornado após uma requisição
-        de remoção.
-    """
+    
     mesage: str
     nome: str
 
 def apresenta_produto(produto: Produto):
-    """ Retorna uma representação do produto seguindo o schema definido em
-        ProdutoViewSchema.
-    """
+    
     return {
         "id": produto.id,
         "nome": produto.nome,
