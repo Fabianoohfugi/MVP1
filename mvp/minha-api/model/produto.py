21ad0338edd1d0dd275223117d46bc65dfbb55c9
Fabianoohfugi <71031819+Fabianoohfugi@users.noreply.github.com>
2023-05-02 04:14:43 -0300

    Update produto.py

--- mvp/minha-api/model/produto.py
+++ mvp/minha-api/model/produto.py
@@ -10,21 +10,11 @@ class Produto(Base):
     quantidade = Column(Integer)
     valor = Column(Float)
 
-    # Definição do relacionamento entre o produto e o comentário.
-    # Essa relação é implicita, não está salva na tabela 'produto',
-    # mas aqui estou deixando para SQLAlchemy a responsabilidade
-    # de reconstruir esse relacionamento.
+    
     comentarios = relationship("Comentario")
 
     def __init__(self, nome: str, quantidade: int, valor: float):
-        """
-        Cria um Produto
-
-        Arguments:
-            nome: nome do produto.
-            quantidade: quantidade que se espera comprar daquele produtona
-            valor: valor esperado para o produto
-        """
+      
         self.nome = nome
         self.quantidade = quantidade
         self.valor = valor
