1e12e15d4f761aefdf75035476459bbf6c718151
Fabianoohfugi <71031819+Fabianoohfugi@users.noreply.github.com>
2023-05-02 04:28:04 -0300

    Update comentario.py

--- mvp/minha-api/schemas/comentario.py
+++ mvp/minha-api/schemas/comentario.py
@@ -5,4 +5,4 @@ class ComentarioSchema(BaseModel):
     """ Define como um novo comentário a ser inserido deve ser representado
     """
     produto_id: int = 1
-    texto: str = "Só comprar se o preço realmente estiver bom!"
+    texto: str = "compre se quiser"
